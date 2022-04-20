a0abb7e3356cefadf71f9c72f11ebe35079b9773
grs4447 <103640572+grs4447@users.noreply.github.com>
2022-04-20 12:42:06 +0530

    Add files via upload

---
+++ demoGitSample/demoGitSample/src/main/java/com/example/demoGitSample/DemoGitSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoGitSample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGitSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoGitSampleApplication.class, args);
+	}
+
+}
---
+++ demoGitSample/demoGitSample/src/test/java/com/example/demoGitSample/DemoGitSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoGitSample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGitSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
